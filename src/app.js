451b92c4ad7b2f10686e4f3c8873cccc8fa6c5fe
Ryan Lima <ryanlima08021997@gmail.com>
2020-07-11 01:04:32 -0300

    Atualiza origin

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: process.env.FRONT_URL || "http://localhost:8080",
+		origin: process.env.FRONT_URL,
 	})
 );
 
